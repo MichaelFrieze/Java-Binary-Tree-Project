fc9b65a0c04be99994c3c5e5f3e9c39f071127be
Michael Frieze <michaelfrieze@icloud.com>
2016-08-05 23:35:35 -0400

    Initial Commit

---
+++ cse231/source/edu/oakland/helper/ArrayGen.java
@@ -0,0 +1,171 @@
+package edu.oakland.helper;
+
+import java.util.*;
+
+/**
+ * This class is responsible for generating arrays of type int. The arrays
+ * represent the values of the keys and data contained in the nodes of a
+ * binary tree.
+ *
+ * @author Serar Matloob, Michael Frieze, Tiantian Ma, Jesse Emelian
+ * @version 1.0 - Date: 160407
+ * @since 1.0
+ */
+public class ArrayGen {
+
+    /**
+     * Instance variables and initial maximum and minimum values
+     * of possible random numbers for each element in an array.
+     */
+    private int arraySize;
+    private int maxValue = 20000; // default value
+    private int minValue = 20; // default value
+    private int[] keyArray;
+    private int[] dataArray;
+    private Random random;
+
+    /**
+     * Constructor assigns a parameter to an instance variable that represents the size
+     * of the array to be generated and creates a Random() object that is assigned to
+     * a reference that exist as an instance variable.
+     *
+     * @param arraySize A value of type int that represents the size of an array to build
+     */
+    public ArrayGen(int arraySize) {
+        this.arraySize = arraySize;
+        this.random = new Random();
+    }
+
+    /**
+     * This method creates an int array that contains random values to be used as keys
+     * for nodes in a binary tree. The conditions of this array must be that no
+     * duplicates are created. The int array object is created, filled, and stored
+     * in a reference that exist as an instance variable.
+     * <p>
+     * Also, the purpose of using LinkedhashSet to fill the int array, is
+     * because of the usefullness of the .add() method. It prevents duplicates.
+     */
+    public void createNodeKeys() {
+        Integer next;
+        Set<Integer> generated = new LinkedHashSet<Integer>();
+        this.keyArray = new int[arraySize];
+        int index = 0;
+
+        while (generated.size() < arraySize) {
+
+            next = random.nextInt(arraySize);
+            generated.add(next);
+        }
+        for (Integer key : generated) {
+
+            keyArray[index++] = key;
+        }
+    }
+
+    /**
+     * This method creates an int array that contains random values to be used as the
+     * data for nodes in a binary tree. The int array object is created, filled, and
+     * stored in a reference that exist as an instance variable.
+     * <p>
+     * The range of possible values in each element of
+     * the int array are determined by the instance variables maxValue and minValue.
+     */
+    public void createRandomDataArray() {
+
+        this.dataArray = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+
+            this.dataArray[i] = random.nextInt(maxValue) + minValue;
+        }
+
+    }
+
+    /**
+     * This method creates an int array that contains random EVEN values to be used as
+     * the data for nodes in a binary tree. The int array object is created, filled, and
+     * stored in a reference that exist as an instance variable.
+     * <p>
+     * The range of possible values in each element of
+     * the int array are determined by the instance variables maxValue and minValue.
+     */
+    public void createEvenDataArray() {
+
+        int[] evenArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            int number = random.nextInt(maxValue) + minValue;
+            if (number % 2 == 0) {
+                evenArray[i] = number;
+            } else {
+                evenArray[i] = number + 1;
+            }
+        }
+        dataArray = evenArray;
+    }
+
+    /**
+     * This method creates an int array that contains random ODD values to be used as
+     * the data for nodes in a binary tree. The int array object is created, filled, and
+     * stored in a reference that exist as an instance variable.
+     * <p>
+     * The range of possible values in each element of
+     * the int array are determined by the instance variables maxValue and minValue.
+     */
+    public void createOddDataArray() {
+
+        int[] oddArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            int number = random.nextInt(maxValue) + minValue;
+            if (number % 2 != 0) {
+                oddArray[i] = number;
+            } else {
+                oddArray[i] = number + 1;
+            }
+        }
+        dataArray = oddArray;
+    }
+
+    /**
+     * This method returns a referene to an array object of type int that
+     * represents keys for a node in a binary tree.
+     *
+     * @return Returns the reference to an array object of type int containing node keys
+     */
+    public int[] getKeyArray() {
+
+        return keyArray;
+    }
+
+    /**
+     * This method returns a referene to an array object of type int that
+     * represents data for a node in a binary tree.
+     *
+     * @return Returns the reference to an array object of type int containing data for nodes
+     */
+    public int[] getDataArray() {
+
+        return dataArray;
+    }
+
+    /**
+     * This method assigns the minimum int value of possible random numbers
+     * to a reference that exist as an instance variable.
+     *
+     * @param minimum A value of type int that gets assigned to an instance variable
+     */
+    public void setMinValue(int minimum) {
+
+        this.minValue = minimum;
+    }
+
+    /**
+     * This method assigns the maximum int value of possible random numbers
+     * to a reference that exist as an instance variable.
+     *
+     * @param maximum A value of type int that gets assigned to an instance variable
+     */
+    public void setMaxValue(int maximum) {
+
+        this.maxValue = maximum;
+    }
+}
---
+++ cse231/source/edu/oakland/test/BinaryTreeTest.java
@@ -0,0 +1,92 @@
+package edu.oakland.test;
+
+import edu.oakland.production.*;
+import edu.oakland.helper.*;
+import junit.framework.*;
+
+/**
+ * This is a JUnit Test class that will test BinaryTree class and checks if it searches and returns
+ * the first two odd integers within the binaryTree and within given range.
+ *
+ * @author Serar Matloob, Michael Frieze, Tiantian Ma, Jesse Emelian
+ * @version 1.0 - Date: 160407
+ * @since 1.0
+ */
+
+public class BinaryTreeTest extends TestCase {
+    /**
+     * *Instance Variables
+     * The variable binaryTree of type BinaryTree is used to hold a reference
+     * to a BinaryTree object for scope purposes.
+     * <p>
+     * The variable arrayGen of type ArrayGen is used to hold a reference
+     * to a ArrayGen object for scope purposes.
+     */
+    private BinaryTree binaryTree;
+    private ArrayGen arrayGen;
+    private int[] keys, data;
+    private int startRange, endRange;
+    private int arraySize;
+
+    /**
+     * This method initializes the test by creating the test arrays (Keys and Data) within a given size
+     * and passing them to the BinaryTree.
+     */
+    public void setUp() {
+
+        arraySize = 20000; // number of array elements
+        startRange = 900; // start of range for the value an odd number must have
+        endRange = 9000; // end of range for the value an odd number must have;
+
+        arrayGen = new ArrayGen(arraySize);
+        arrayGen.createNodeKeys();
+        arrayGen.createRandomDataArray();
+        keys = arrayGen.getKeyArray();
+        data = arrayGen.getDataArray();
+
+        binaryTree = new BinaryTree(keys, data);
+
+    }
+
+    /**
+     * This method starts the test by calling buildBinaryTree() method to build the binaryTree and then calls
+     * searchOddPreOrder() method to search for first two odds using preorderTraversel method.
+     * <p>
+     * If the returned array from the search contains the two odd integers within the given range then it returns a boolean true.
+     */
+    public void testBinaryTree() {
+        binaryTree.buildBinaryTree();
+        binaryTree.searchOddPreOrder();
+        // binaryTree.searchOddPostOrder();
+        // binaryTree.searchOddInOrder();
+
+        System.out.println();
+        System.out.format("The size of the array is %,d elements\n", arraySize);
+
+        System.out.println("\nArray search time:");
+        System.out.println(binaryTree.getTime() + " nanoseconds");
+        System.out.println((binaryTree.getTime() / 1000) + " microseconds");
+        System.out.println((binaryTree.getTime() / 1000000) + " milliseconds\n");
+
+
+        int[] oddNumber = binaryTree.getValues();
+
+        System.out.print("Odd numbers: ");
+
+        for (int i = 0; i < oddNumber.length; i++) {
+            System.out.print(" [ " + oddNumber[i] + " ] ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        boolean isOddInRange;
+
+        for (int i = 0; i < oddNumber.length; i++) {
+            isOddInRange = (oddNumber[i] % 2 != 0 && oddNumber[i] >= startRange && oddNumber[i] <= endRange);
+            assertTrue(isOddInRange);
+        }
+
+    }
+
+}
